5ea99ca904c2a276d9d82f13784e2d3e5e4385ce
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-09-07 14:16:46 +0100

    Tidy up tests

--- tests/add.contact.test.js
+++ tests/add.contact.test.js
@@ -10,13 +10,12 @@ describe('Contact Manager app tests', () => {
         browser.setValue(acp.contactPhoneEditText, "0131 254785");
         browser.setValue(acp.contactEmailEditText, "jo.bloggs@gmail.com");
         browser.element(acp.contactEmailDropdown).click();
-        browser.pause(500); // wait for popup.
+        browser.waitForVisible(acp.contactEmailOptionWork);
         browser.element(acp.contactEmailOptionWork).click();
-
-
         browser.element(acp.saveBtn).click();
 
         // Show/Hide invisible contacts.
+        browser.waitForVisible(clp.showInvisibleBox)
         browser.click(clp.showInvisibleBox);
         browser.click(clp.showInvisibleBox);
 
